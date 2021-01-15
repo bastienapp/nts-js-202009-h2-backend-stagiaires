ef5929b71191a367cf463add197c402a817662a9
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-01-15 12:21:29 +0100

    fix

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const root = require('./routes/index');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const { CLIENT_URL } = process.env;
+
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors({ origin: CLIENT_URL }));
 app.use(express.json());
 app.use(
   express.urlencoded({
