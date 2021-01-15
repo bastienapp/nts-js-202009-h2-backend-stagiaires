772c40bb70e127d674829288879055f1611a7185
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-01-15 11:49:07 +0100

    fix cors

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const root = require('./routes/index');
 const port = process.env.PORT || 8080;
 const app = express();
 
-const { CLIENT_URL } = process.env;
-
 app.use(logger('dev'));
-app.use(cors({ origin: CLIENT_URL }));
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
