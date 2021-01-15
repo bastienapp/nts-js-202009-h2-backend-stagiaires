883dd9845d83fac8f5092a70239d4d6662364604
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-01-15 11:33:40 +0100

    revert

--- controllers/annonce.js
+++ controllers/annonce.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const authenticateWithJsonWebToken = require('../middleware');
 const Annonce = require('../models/Annonce');
 
-router.get('/', async (req, res) => {
+router.get('/', authenticateWithJsonWebToken, async (req, res) => {
   const { langage } = req.query;
   try {
     const allAnnonces = await Annonce.findAllAnnonces(langage);
